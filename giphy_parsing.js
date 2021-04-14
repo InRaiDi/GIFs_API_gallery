1ef3ee6b0fae01917cfc50d8b29acf1b4f07835f
Dinara <inraidi@gmail.com>
2021-04-14 18:45:13 -0400

    UI: flex grid + added titles and links

--- giphy_parsing.js
+++ giphy_parsing.js
@@ -1,5 +1,5 @@
 	// getting data
-	var xhr = $.get("http://api.giphy.com/v1/gifs/trending?api_key=jX4avpPN5rTB9wXq9lVszyjXo6LD97nq&limit=10");
+	var xhr = $.get("http://api.giphy.com/v1/gifs/trending?api_key=jX4avpPN5rTB9wXq9lVszyjXo6LD97nq&limit=15");
 	xhr.done(function(response) { 
 		console.log("success got data", response);
 
@@ -8,12 +8,20 @@ for (var i=0; i<Object.keys(response.data).length; i++){
             var gifBlock = $("<div>"); 
             gifBlock.addClass("gifBlock");
        
+            //gif image itself
             var gifImg = $("<img>");
             gifImg.attr("src", response.data[i].images.fixed_height.url);
             gifImg.addClass("gif_img");
             gifBlock.append(gifImg);
 
+            //gif title
+            var gifTitle = $("<div>").append( response.data[i].title); 
+            gifTitle.addClass("gifTitle");
+            gifBlock.append(gifTitle);
+
+
             $("#main_container").append(gifBlock);
+             gifBlock.wrap( "<a href="+response.data[i].url+ "></a>" );
         }
 
 
